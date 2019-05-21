388b539255df3c6710cdddba6babc9c746a25b87
Diego Mejia <redwallsounds@gmail.com>
2019-05-20 18:43:28 -0700

    Add Bluebird promise library and add structure comments

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 "use strict";
 
+//Library requirements
 const express = require('express');
 const fs = require('fs');
+const Promise = require('bluebird');
+
+// Variables
 const app = express();
 const port = 2000;
 let passwd = { buffer: "default value",
                location: "/etc/passwd"
              };
 
+// Function Definitions
 let logPortNumber = function(){
   console.log('passwd_service listening on port ' + port.toString())
 };
@@ -26,7 +31,7 @@ let serviceGetRequest = function(request, response){
   response.send(passwd.buffer);
 }
 
-
+// Function Calls
 // Open and read file with UTF-8 encoding
 fs.readFile(passwd.location, {encoding:'utf-8', flag:'r'}, readCallback);
 
