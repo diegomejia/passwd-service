ab5839161f22dabb65cd8d78ec28a42dd0b05052
Diego Mejia <redwallsounds@gmail.com>
2019-05-22 05:42:36 -0700

    GET /users now responds with correct default response, query scaffolding added

--- index.js
+++ index.js
@@ -28,56 +28,6 @@ let readCallback = function(error, data){
 let serviceGetRequest = function(request, response){
   //fs.readFile(passwd.location, {encoding: 'utf-8', flag: 'r'}, readCallback);
 
-   let file = readFile(passwd.location, 'utf-8').then(
-     function(data){
-       let tmpDataBuffer = "";
-
-       let passwdArray = data.split("\n");
-       let filteredPasswdArray = [];
-       passwdArray.forEach(function(line){
-         //remove comments(lines beginning with '#') and any empty lines
-         if(line.charAt(0) != "#" && line != ""){
-            filteredPasswdArray.push(line);
-        }
-       });
-
-       let filteredUsersArray = [];
-       // Expected format for each line, 7 values
-       // username:password:user_id:group_id:user_id_info:home_directory:shell
-       filteredPasswdArray.forEach(function(line){
-         let values = [];
-         let user = {
-           name: "",
-           uid: "",
-           gid: "",
-           comment: "",
-           home: "",
-           shell: ""
-         };
-         values = line.split(":"); //passwd and group files are colon delimited
-         if(values.length == 7){
-           //construct users object
-           user.name = values[0];
-           // skip password field values[1]
-           user.uid = values[2];
-           user.gid = values[3];
-           user.comment = values[4];
-           user.home = values[5];
-           user.shell = values[6];
-
-           //push onto Object array
-           filteredUsersArray.push(user);
-         }
-       });
-       tmpDataBuffer = JSON.stringify(filteredUsersArray);
-       //console.log(data);
-       passwd.buffer = tmpDataBuffer;
-       return tmpDataBuffer;
-     }
-   ).catch( function(err){
-     console.log("Error attempting to read file. Location does not exist.")
-     return "Error: Error attempting to read file. Location does not exist.";
-   } );
    // if(file != null){
    //   debugger;
    //   console.log(file);
@@ -86,8 +36,63 @@ let serviceGetRequest = function(request, response){
 };
 
 let serviceUsersRequest = function(request, response){
-  // Scaffolding..
-  response.send('Users Request Received.')
+  let file = readFile(passwd.location, 'utf-8').then(
+       function(data){
+         let tmpDataBuffer = "";
+
+         let passwdArray = data.split("\n");
+         let filteredPasswdArray = [];
+         passwdArray.forEach(function(line){
+           //remove comments(lines beginning with '#') and any empty lines
+           if(line.charAt(0) != "#" && line != ""){
+              filteredPasswdArray.push(line);
+          }
+         });
+
+         let filteredUsersArray = [];
+         // Expected format for each line, 7 values
+         // username:password:user_id:group_id:user_id_info:home_directory:shell
+         filteredPasswdArray.forEach(function(line){
+           let values = [];
+           let user = {
+             name: "",
+             uid: "",
+             gid: "",
+             comment: "",
+             home: "",
+             shell: ""
+           };
+           values = line.split(":"); //passwd and group files are colon delimited
+           if(values.length == 7){
+             //construct users object
+             user.name = values[0];
+             // skip password field values[1]
+             user.uid = values[2];
+             user.gid = values[3];
+             user.comment = values[4];
+             user.home = values[5];
+             user.shell = values[6];
+
+             //push onto Object array
+             filteredUsersArray.push(user);
+           }
+         });
+         tmpDataBuffer = JSON.stringify(filteredUsersArray);
+         //console.log(data);
+         passwd.buffer = tmpDataBuffer;
+         return tmpDataBuffer;
+       }
+     ).catch( function(err){
+       console.log("Error attempting to read file. Location does not exist.")
+       return "Error: Error attempting to read file. Location does not exist.";
+     } );
+  debugger;
+  response.send(passwd.buffer);
+};
+
+let serviceGetUsersQueryRequest = function(request, response){
+  //Scaffolding
+  response.send("Query Request Received");
 };
 // Function Calls
 // Open and read file with UTF-8 encoding
@@ -95,5 +100,6 @@ let serviceUsersRequest = function(request, response){
 
 app.get('/', serviceGetRequest);
 app.get('/users', serviceUsersRequest);
+app.get('/users/query', serviceGetUsersQueryRequest);
 
 app.listen(port, logPortNumber);
