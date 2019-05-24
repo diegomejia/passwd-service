7a9af7553c1b1c871aa9b684904756d4bc8539d2
Diego Mejia <diegomejia87@gmail.com>
2019-05-24 03:51:00 -0700

    Improve test output

--- tests/getUsersQuery.js
+++ tests/getUsersQuery.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'GET /users/ Test' : function (browser) {
+  'GET /users/query? Test' : function (browser) {
     browser
       .url('http://localhost:2000/users/query?name=nobody&uid=-2&gid=-2&comment=Unprivileged+User&home=%2Fvar%2Fempty&shell=%2Fusr%2Fbin%2Ffalse')
       .waitForElementVisible('body')
